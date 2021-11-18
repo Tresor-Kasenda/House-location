0bb3ea465df15eec0bd195ce3e5921e4e93ee122
Scott Tresor <scotttresor@gmail.com>
2021-11-18 17:18:38 +0100

    [FEAT] review category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use JustSteveKing\KeyFactory\Models\Concerns\HasKey;
 
 class Category extends Model
@@ -13,4 +14,14 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function houses(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            House::class,
+            'house_category',
+            'house_id',
+            'category_id'
+        )->withTimestamps();
+    }
 }
