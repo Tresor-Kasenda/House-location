fbcf91626af62e5b7418a8f1a59e6425ea9d2c42
Scott Tresor <scotttresor@gmail.com>
2021-11-18 07:03:34 +0100

    [FEAT] reviews migrations

--- database/migrations/2021_11_17_130515_create_categories_table.php
+++ database/migrations/2021_11_17_130515_create_categories_table.php
@@ -1,16 +1,12 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
@@ -21,13 +17,8 @@ class CreateCategoriesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('categories');
     }
-}
+};
--- database/migrations/2021_11_17_133352_create_house_category_table.php
+++ database/migrations/2021_11_17_133352_create_house_category_table.php
@@ -1,33 +1,24 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHouseCategoryTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('house_category', function (Blueprint $table) {
             $table->id();
-            $table->string('house_key');
-            $table->string('category_key');
+            $table->foreignId('house_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('house_category');
     }
-}
+};
