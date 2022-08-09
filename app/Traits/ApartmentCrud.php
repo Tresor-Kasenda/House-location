ca9b54071dda4db15f1dd51e6632c2cb5c983621
scott Tresor <scotttresor@gmail.com>
2022-08-09 06:19:04 +0200

    [fi] reservation

--- app/Traits/ApartmentCrud.php
+++ app/Traits/ApartmentCrud.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
-use App\Enums\HouseEnum;
 use App\Models\Detail;
-use App\Models\House;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -32,14 +30,7 @@ trait ApartmentCrud
             'type_id' => $attributes->input('type')
         ]);
         $apartment->categories()->attach($attributes->categories);
-        Detail::query()
-            ->create([
-                'house_id' => $apartment->id,
-                'room_number' => $attributes->input('room_number'),
-                'number_pieces' => $attributes->input('room_pieces'),
-                'toilet' => $attributes->input('toilet'),
-                'electricity' => $attributes->input('electricity')
-            ]);
+        $this->createDetails($apartment, $attributes);
         $this->service->success(
             messages: "Un nouveau appartement à été ajouter",
             type: "success"
@@ -54,24 +45,36 @@ trait ApartmentCrud
         $house->categories()->detach($attributes->categories);
         $house->update([
             'prices' => $attributes->input('prices'),
+            'commune'=> $attributes->input('commune'),
+            'town' => $attributes->input('town'),
+            'district'=> $attributes->input('district'),
             'address'=> $attributes->input('address'),
             'guarantees'=> $attributes->input('guarantees'),
-            'phone_number'=> $attributes->input('phoneNumber'),
+            'phone_number'=> $attributes->input('phone_number'),
             'email'=> $attributes->input('email'),
             'latitude'=> $attributes->input('latitude'),
             'longitude'=> $attributes->input('longitude'),
-            'commune'=> $attributes->input('commune'),
-            'district'=> $attributes->input('district'),
             'images'=> $this::uploadFiles($attributes),
-            'roomNumber'=> $attributes->roomNumber,
-            'town' => $attributes->town,
             'type_id' => $attributes->input('type')
         ]);
         $house->categories()->attach($attributes->categories);
+
         $this->service->success(
             messages: "Un nouveau appartement à été modifier",
             type: "success"
         );
         return $house;
+    }
+
+    public function createDetails($apartment, $attributes)
+    {
+         Detail::query()
+            ->create([
+                'house_id' => $apartment->id,
+                'room_number' => $attributes->input('room_number'),
+                'number_pieces' => $attributes->input('room_pieces'),
+                'toilet' => $attributes->input('toilet'),
+                'electricity' => $attributes->input('electricity')
+            ]);
     }
 }
