43061866a206fd9a076bce074a02aff5be19595b
scott Tresor <scotttresor@gmail.com>
2022-08-24 03:18:34 +0200

    [FIX] compile results

--- resources/js/components/search.js
+++ resources/js/components/search.js
@@ -1,23 +1,25 @@
 import $ from 'jquery'
 import 'jquery-ui/ui/widgets/autocomplete';
+const search = $('#location').val();
+const render  = $('#resultRender');
 
-$('#location').on('keyup', function () {
-    const search = $('#location').val();
-    const render  = $('#resultRender');
-    if(search.length > 3){
-        $.ajax({
-            type: "GET",
-            url: '/search',
-            data: {_search : search},
-            dataType: 'json',
-            delay: 220,
-            autofocus: true,
-            success: function (response) {
-                if(response.search){
-                    render.html(response.search)
+if (search.length > 3) {
+    search.on('keyup', function () {
+        if(search.length > 3){
+            $.ajax({
+                type: "GET",
+                url: '/search',
+                data: {_search : search},
+                dataType: 'json',
+                delay: 220,
+                autofocus: true,
+                success: function (response) {
+                    if(response.search){
+                        render.html(response.search)
+                    }
+                    render.html(response.empty)
                 }
-                render.html(response.empty)
-            }
-        })
-    }
-})
+            })
+        }
+    })
+}
