738d37d60b5a23f6e0cfbb9ad17d4a76e6531094
scott-Tresor <34010260+Scott-Tresor@users.noreply.github.com>
2022-10-28 12:18:49 +0200

    Fixing (#49)
    * [FIX] confirmation review adminstration systeme
    
    * [FIX] add edit images
    
    * [FIX] review query of client and dealer
    
    * [FIX] affichage de chart js
    
    * [fix] review design system
    
    * [FIX] fix bug to slides
    
    * [FIX] review design systeme
    
    * [FIX] create client and update client
    
    * [FIX] review booking and transaction
    
    * [FEAT] add reservation Command to delete latested reservation
    
    * [FIX] review content management
    
    * [FIX] annulation of and confirmation of reservation
    
    * [FEAT] fix created localization
    
    * [FEAT] fix created localization
    
    * [FEAT] add schedules
    
    * [FEAT] add reservation email
    
    * [FEAT] booking notification, event and listener
    
    * [FEAT] booking notification, event and listener
    
    * [FEAT] booking notification, event and listener
    
    * [FEAT] review booking
    
    * [FEAT] review booking
    
    * [FEAT] review booking
    
    * [FEAT] review booking
    
    * [FEAT] review booking
    
    * [FEAT] review booking

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,17 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-const path = require('path');
-require('laravel-mix-purgecss');
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+const path = require("path");
+require("laravel-mix-purgecss");
 
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/backend/backend.js', 'public/backend')
+mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/backend/backend.js", "public/backend")
     .options({
         processCssUrls: false,
-        postCss: [
-            tailwindcss('./tailwind.config.js')
-        ],
+        postCss: [tailwindcss("./tailwind.config.js")],
         use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-                presets: ['@babel/preset-env'],
+                presets: ["@babel/preset-env"],
             },
         },
     })
@@ -22,10 +20,9 @@ mix.js('resources/js/app.js', 'public/js')
             children: true,
         },
     })
-    .postCss('resources/css/backend.css', 'public/backend')
-    .postCss('resources/css/app.css', 'public/css', [
-        require("tailwindcss"),
-    ]).purgeCss()
+    .postCss("resources/css/backend.css", "public/backend")
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .purgeCss()
     .alias({
-        ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+        ziggy: path.resolve("vendor/tightenco/ziggy/dist"),
     });
